c9a7997c0dc754bf6e5b10f030a6a9d9d547905a
mitsuru793 <mitsuru793@gmail.com>
2019-06-09 23:18:05 +0900

    style: fix lint

--- src/__tests__/TemplateTest.ts
+++ src/__tests__/TemplateTest.ts
@@ -1,7 +1,7 @@
-import Template from "../Template"
+import Template from '../Template'
 
 describe('Template', (): void => {
-  it('can render readme', async () => {
+  it('can render readme', async (): Promise<void> => {
     const text = await Template.readme('to play music')
     expect(text).toMatchSnapshot()
   })
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 import Template from './Template'
 
-export {Template}
+export { Template }
